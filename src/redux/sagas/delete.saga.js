290d25fee0f0f4edf6d26fd6d5ec350bf39a3592
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-02-04 11:46:33 -0600

    delete request with config

--- src/redux/sagas/delete.saga.js
+++ src/redux/sagas/delete.saga.js
@@ -1,22 +1,31 @@
-
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-
 function* deleteItem(action) {
-    console.log("in delete", action.payload);
-  
+  console.log("in delete", action.payload);
+
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    // send the action.payload as the body
+    // the config includes credentials which
+    // allow the server session to recognize the user
     // delete element data to the server
-    yield axios.delete(`/api/shelf/${action.payload}`);
-  
+    yield axios.delete(`/api/shelf/${action.payload}`, config);
+
     // Run the fetch
     yield put({
       type: "FETCH_ITEM",
     });
+  } catch (error) {
+    console.log("User delete request failed", error);
   }
+}
 
+function* deleteSaga() {
+  yield takeLatest("DELETE_ITEMS", deleteItem);
+}
 
-  function* deleteSaga() {
-    yield takeLatest("DELETE_ITEMS", deleteItem);
-  }
 export default deleteSaga;
